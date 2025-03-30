78fde3b1dd0831cc767c767677af308416d73343
Adwithr <154674132+Adwithr@users.noreply.github.com>
2025-03-30 22:50:13 +0530

    initialize for new user

--- src/modules/init.js
+++ src/modules/init.js
@@ -4,7 +4,34 @@ import { displayTask, displayProjects } from "./domUtils";
 import { loadFromLocalStorage } from "./localStorageHelper";
 
 export default function initializeApp() {
-  const projectArr = loadFromLocalStorage("projectArr");
+  let projectArr = loadFromLocalStorage("projectArr");
+  if (projectArr.length === 0) {
+    projectArr = [
+      {
+        title: "Welcome to Todo List",
+        todos: [
+          {
+            title: "Explore the Features",
+            description: "Take some time to familiarize yourself with the different sections and functionalities of the todo list.",
+            dueDate: "2025-12-31",
+            priority: "high",
+          },
+          {
+            title: "Create Your First Project",
+            description: "Start a new project to organize your tasks based on a specific goal or area of focus.",
+            dueDate: "2025-12-31",
+            priority: "high",
+          },
+          {
+            title: "Add a Task to Your Project",
+            description: "Try adding a task to your newly created project. You can add details like due dates and priorities.",
+            dueDate: "2025-12-31",
+            priority: "high",
+          },
+        ],
+      },
+    ];
+  }
   Project.setProjectArr(projectArr);
 
   const completedProjectsArr = loadFromLocalStorage("compProjArray");
